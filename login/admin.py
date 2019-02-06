10a329ac1abccf83946d2ac9a5a65d09c89059bb
San <jtlsan@naver.com>
2019-02-06 15:31:09 +0900

    Models for UserInfo, Unbrella, StudyTable

--- login/admin.py
+++ login/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+
+admin.site.register(models.Unbrella)
+admin.site.register(models.UserInfo)
+admin.site.register(models.StudyTable)
--- login/models.py
+++ login/models.py
@@ -6,8 +6,38 @@ class UserInfo(models.Model):
     name = models.CharField(max_length = 10)
     stdID = models.CharField(max_length = 8)
     HB = models.BooleanField(default = False)
+    today_A4 = models.PositiveIntegerField(default = 0)
+    month_A4 = models.PositiveIntegerField(default = 0)
+    battery = models.BooleanField(default = False)
+    Lan = models.BooleanField(default = False)
+    unbrella_borrowed = models.BooleanField(default = False)
+    table_borrowed = models.BooleanField(default = False)
+    StartTime = models.TimeField(blank=True, null=True)
+    EndTime = models.TimeField(blank=True, null=True)
 
     def __str__(self):
         return self.name
     
     objects = models.Manager()
+
+
+class Unbrella(models.Model):
+    number = models.PositiveSmallIntegerField()
+    borrowed = models.BooleanField(default = False)
+    borrowed_by = models.OneToOneField(UserInfo, related_name = "unbrella", null=True, blank=True, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.number)
+
+
+class StudyTable(models.Model):
+    number = models.PositiveSmallIntegerField()
+    borrowed = models.BooleanField(default = False)
+    borrowed_by = models.OneToOneField(UserInfo, related_name = "table", null=True, blank=True, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "Table "+str(self.number)
+        
+
+
+
