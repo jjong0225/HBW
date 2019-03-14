8d67e63ddf954545ee80874555021b8c27c0feb7
jjong0225 <jjong0225@naver.com>
2019-03-14 18:58:48 +0900

    Update views.py

--- login/views.py
+++ login/views.py
@@ -7,6 +7,11 @@ from django.contrib.auth import login, authenticate
 from django.template import RequestContext
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
+from django.utils import timezone
+import datetime
+from django.db.models import Q
+import logging
+logger = logging.getLogger(__name__)
 
 
 from urllib.parse import urlparse, urlunparse
@@ -41,27 +46,45 @@ from django.views.generic.edit import FormView
 
 
 
+class UserHandlingClass () :
+    # 'name', 'stdID', 'HB'
+    def Signup(request):
+        if request.method == "POST":
+            form = UserForm(request.POST)
+            if form.is_valid():
+                new_user = User.objects.create_user(**form.cleaned_data)
+                login(request, new_user)
+                return redirect('login:main')
+        else:
+            form = UserForm()
+            return render(request, 'login/signup.html', {'form': form})
+
+    def Signin(request):
+        if request.method == "POST":
+            form = UserForm(request.POST)
+            username = request.POST['username']
+            password = request.POST['password']
+            user = authenticate(username = username, password = password)
+            if user is not None:
+                login(request, user)
+                return redirect('login:main')
+            else:
+                return HttpResponse(user)
+        else:
+            form = UserForm()
+            return render(request, 'login/final_login.html', {'form': form})
+
+
 def Main(request):
+
     unbrella_set = models.Unbrella.objects.all()
     battery_set = models.Battery.objects.all()
     lan_set = models.Lan.objects.all()
     post_q = models.Poster.objects.all().order_by('number')
-    unbrella_count = 0
-    battery_count = 0
-    lan_count = 0
+    unbrella_count = unbrella_set.filter(Q(is_borrowed = True) | Q(is_reserved = True)).count()
+    battery_count = battery_set.filter(Q(is_borrowed = True) | Q(is_reserved = True)).count()
+    lan_count = lan_set.filter(Q(is_borrowed = True) | Q(is_reserved = True)).count()
     
-    
-    for item in unbrella_set:
-        if item.is_borrowed:
-            unbrella_count = unbrella_count + 1
-    for item in battery_set:
-        if item.is_borrowed:
-            battery_count = battery_count + 1
-    for item in lan_set:
-        if item.is_borrowed:
-            lan_count = lan_count + 1
-    
-
     return render(request, 'login/final_main.html', {
         'battery_count': battery_count,
         'unbrella_count': unbrella_count,
@@ -71,70 +94,6 @@ def Main(request):
         'unbrella_total': unbrella_set.count(),
         'lan_total': lan_set.count(),
     })
-
-
-
-# 'name', 'stdID', 'HB'
-def Signup(request):
-    if request.method == "POST":
-        form = UserForm(request.POST)
-        if form.is_valid():
-            new_user = User.objects.create_user(**form.cleaned_data)
-            login(request, new_user)
-            return redirect('login:main')
-    else:
-        form = UserForm()
-        return render(request, 'login/signup.html', {'form': form})
-
-def Signin(request):
-    if request.method == "POST":
-        form = UserForm(request.POST)
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(username = username, password = password)
-        if user is not None:
-            login(request, user)
-            return redirect('login:main')
-        else:
-            return HttpResponse(user)
-    else:
-        form = UserForm()
-        return render(request, 'login/final_login.html', {'form': form})
-
-
-
-def TableLend(request):
-    tables = StudyTable.objects.all()
-    return render(request, 'login/place_reservation.html', {'tables':tables})
-
-def TableSelect(request):
-    if request.method == "POST":
-        if request.POST.get('desk') is not None:
-            selected_number = request.POST['desk']
-            table_q = StudyTable.objects.all().filter(number=selected_number)
-            return render(request, 'login/place_reservation.html', {'tables' : table_q})
-        else:
-            form = TableForm()
-            return render(request, 'login/place_reservation.html', {'form': form})            
-    else:
-        form = TableForm()
-        return render(request, 'login/place_reservation.html', {'form': form})
-
-def LendTable(request):
-    if request.method == "POST":
-        sel_time = request.POST.getlist('time[]')
-        sel_table = request.POST.getlist('number[]')
-        table_q = StudyTable.objects.all().filter(start_time__in=sel_time).filter(number__in=sel_table)
-        for sel in table_q:
-                sel.is_borrowed = True
-                sel.lender = request.user.user_data
-                sel.save()
-        return redirect('login:seltable')
-    else:
-        form = TimeForm()
-        return render(request, 'login/place_reservation.html', {'form' : form})
-
-
 
 #마이페이지
 @login_required
@@ -154,165 +113,172 @@ def MyPage(request):
         time_q = StudyTable.objects.all().filter(lender_id=current_user.id)
         return render(request, 'login/mypage.html', {'times' : time_q})
 
-#우산대여
-@login_required
-def LendUnbrella(request):
-    unbrella_set = models.Unbrella.objects.all()
-    battery_set = models.Battery.objects.all()
-    lan_set = models.Lan.objects.all()
-    unbrella_count = 0
-    battery_count = 0
-    lan_count = 0
-    for unbrella in unbrella_set:
-        if unbrella.is_borrowed:
-            unbrella_count = unbrella_count + 1
-    for item in battery_set:
-        if item.is_borrowed:
-            battery_count = battery_count + 1
-    for item in lan_set:
-        if item.is_borrowed:
-            lan_count = lan_count + 1
 
-    ans=request.POST.get('ans', 'No')
-    if unbrella_count < unbrella_set.count():
-        for item in unbrella_set:
-                if not item.is_borrowed:
-                    break
-        if request.method == "GET":
-            message = str(item.number)+"번 우산을 빌리시겠습니까?"
-            return render(request, 'login/main_lendunbrella.html', {
-                'message': message,
-                'yesno':True,
-                'battery_count':battery_count,
-                'lan_count':lan_count,
-                'battery_total' : battery_set.count(),
-                'unbrella_total': unbrella_set.count(),
-                'lan_total': lan_set.count(),
-            })
+class StudyTableClass() :
+    def TableLend(request):
+        tables = StudyTable.objects.all()
+        return render(request, 'login/place_reservation.html', {'tables':tables})
+
+    def TableSelect(request):
+        if request.method == "POST":
+            if request.POST.get('desk') is not None:
+                selected_number = request.POST['desk']
+                table_q = StudyTable.objects.all().filter(number=selected_number)
+                return render(request, 'login/place_reservation.html', {'tables' : table_q})
+            else:
+                form = TableForm()
+                return render(request, 'login/place_reservation.html', {'form': form})            
+        else:
+            form = TableForm()
+            return render(request, 'login/place_reservation.html', {'form': form})
+
+    def LendTable(request):
+        if request.method == "POST":
+            sel_time = request.POST.getlist('time[]')
+            sel_table = request.POST.getlist('number[]')
+            table_q = StudyTable.objects.all().filter(start_time__in=sel_time).filter(number__in=sel_table).filter(is_borrowed=False)
+            for sel in table_q:
+                    sel.is_borrowed = True
+                    sel.lender = request.user.user_data
+                    sel.save()
+                    logger.info('실습실 사업 : [학번:'+request.user.username+'| 실습실 테이블 번호:'+str(sel.number)+'| 실습실 대여 시간:'+ str(sel.start_time)+'] 예약 완료') # 담당자:{}
+            return redirect('login:seltable')
+        else:
+            form = TimeForm()
+            return render(request, 'login/place_reservation.html', {'form' : form})
+
+
+
+class LendBusinessClass() :
+    #우산대여
+    @login_required
+    def LendUnbrella(request):
+        unbrella_set = models.Unbrella.objects.all()
+        battery_set = models.Battery.objects.all()
+        lan_set = models.Lan.objects.all()
+        post_q = models.Poster.objects.all().order_by('number')
+        unbrella_count = unbrella_set.filter(Q(is_borrowed = True) | Q(is_reserved = True)).count()
+        battery_count = battery_set.filter(Q(is_borrowed = True) | Q(is_reserved = True)).count()
+        lan_count = lan_set.filter(Q(is_borrowed = True) | Q(is_reserved = True)).count()
+        message = ""
+        yes_no = False
+        ans=request.POST.get('ans', 'No')
+        if unbrella_count < unbrella_set.count():
+            for item in unbrella_set:
+                    if item.is_borrowed == False and item.is_reserved == False :
+                        break
+            if request.method == "GET":
+                message = str(item.number)+"번 우산을 예약하시겠습니까?"
+                yes_no = True
+            else :
+                if ans=='Yes':
+                    item.borrowed_by = request.user.user_data
+                    item.is_reserved = True
+                    item.reservation_time = timezone.localtime()
+                    item.save()
+                    logger.info('우산 사업 : [학번:'+request.user.username+'|우산 번호:'+str(item.number)+'] 대여 완료') # 담당자:{}
+                return redirect('login:main')
         else :
-            if ans=='Yes':
-                item.borrowed_by = request.user.user_data
-                item.is_borrowed = True
-                item.save()
-            return redirect('login:main')
-    else :
-        message = "현재 대여 가능한 우산이 없습니다."
-        if ans=="OK":
-            return redirect('login:main')
+            message = "현재 예약 가능한 우산이 없습니다."
+            if ans=="OK":
+                return redirect('login:main')
+            yes_no = False
+
         return render(request, 'login/main_lendunbrella.html', {
             'message': message,
-            'yesno': False,
+            'yesno': yes_no,
             'battery_count':battery_count,
             'lan_count':lan_count,
+            'posts':post_q,
             'battery_total' : battery_set.count(),
             'unbrella_total': unbrella_set.count(),
             'lan_total': lan_set.count(),
             })
 
-#배터리대여
-@login_required
-def LendBattery(request):
-    unbrella_set = models.Unbrella.objects.all()
-    battery_set = models.Battery.objects.all()
-    lan_set = models.Lan.objects.all()
-    unbrella_count = 0
-    battery_count = 0
-    lan_count = 0
-    for unbrella in unbrella_set:
-        if unbrella.is_borrowed:
-            unbrella_count = unbrella_count + 1
-    for item in battery_set:
-        if item.is_borrowed:
-            battery_count = battery_count + 1
-    for item in lan_set:
-        if item.is_borrowed:
-            lan_count = lan_count + 1
-    ans=request.POST.get('ans', 'No')
-    if battery_count < battery_set.count():
-        for item in battery_set:
-                if not item.is_borrowed:
-                    break
-        if request.method == "GET":
-            message = str(item.number)+"번 배터리를 빌리시겠습니까?"
-            return render(request, 'login/main_lendbattery.html', {
-                'message': message,
-                'yesno':True,
-                'lan_count':lan_count,
-                'unbrella_count':unbrella_count,
-                'battery_total' : battery_set.count(),
-                'unbrella_total': unbrella_set.count(),
-                'lan_total': lan_set.count(),
-            })
+    #배터리대여
+    @login_required
+    def LendBattery(request):
+        unbrella_set = models.Unbrella.objects.all()
+        battery_set = models.Battery.objects.all()
+        lan_set = models.Lan.objects.all()
+        post_q = models.Poster.objects.all().order_by('number')
+        unbrella_count = unbrella_set.filter(Q(is_borrowed = True) | Q(is_reserved = True)).count()
+        battery_count = battery_set.filter(Q(is_borrowed = True) | Q(is_reserved = True)).count()
+        lan_count = lan_set.filter(Q(is_borrowed = True) | Q(is_reserved = True)).count()
+        message = ""
+        yes_no = False
+        ans=request.POST.get('ans', 'No')
+        if battery_count < battery_set.count():
+            for item in battery_set:
+                    if item.is_borrowed == False and item.is_reserved == False :
+                        break
+            if request.method == "GET":
+                message = str(item.number)+"번 배터리를 빌리시겠습니까?"
+                yes_no = True
+            else :
+                if ans=='Yes':
+                    item.borrowed_by = request.user.user_data
+                    item.is_reserved = True
+                    item.reservation_time = timezone.localtime()
+                    item.save()
+                    logger.info('배터리 사업 : [학번:'+request.user.username+'| 배터리 번호:'+str(item.number)+'] 대여 완료') # 담당자:{}
+                return redirect('login:main')
         else :
-            if ans=='Yes':
-                item.borrowed_by = request.user.user_data
-                item.is_borrowed = True
-                item.save()
-            return redirect('login:main')
-    else :
-        message = "현재 대여 가능한 배터리가 없습니다."
-        if ans=="OK":
-            return redirect('login:main')
+            message = "현재 예약 가능한 배터리가 없습니다."
+            if ans=="OK":
+                return redirect('login:main')
+            yes_no = False
+
         return render(request, 'login/main_lendbattery.html', {
             'message': message,
-            'yesno': False,
+            'yesno': yes_no,
             'lan_count':lan_count,
+            'posts':post_q,
             'unbrella_count':unbrella_count,
             'battery_total' : battery_set.count(),
             'unbrella_total': unbrella_set.count(),
             'lan_total': lan_set.count(),
             })
 
-#랜선대여
-@login_required
-def LendLan(request):
-    unbrella_set = models.Unbrella.objects.all()
-    battery_set = models.Battery.objects.all()
-    lan_set = models.Lan.objects.all()
-    unbrella_count = 0
-    battery_count = 0
-    lan_count = 0
-    for unbrella in unbrella_set:
-        if unbrella.is_borrowed:
-            unbrella_count = unbrella_count + 1
-    for item in battery_set:
-        if item.is_borrowed:
-            battery_count = battery_count + 1
-    for item in lan_set:
-        if item.is_borrowed:
-            lan_count = lan_count + 1
-
-    ans=request.POST.get('ans', 'No')
-    if lan_count < lan_set.count():
-        for item in lan_set:
-                if not item.is_borrowed:
-                    break
-        if request.method == "GET":
-            message = str(item.number)+"번 랜선을 빌리시겠습니까?"
-            return render(request, 'login/main_lendlan.html', {
-                'message': message,
-                'yesno':True,
-                'unbrella_count':unbrella_count,
-                'battery_count':battery_count,
-                'battery_total' : battery_set.count(),
-                'unbrella_total': unbrella_set.count(),
-                'lan_total': lan_set.count(),
-            })
+    #랜선대여
+    @login_required
+    def LendLan(request):
+        unbrella_set = models.Unbrella.objects.all()
+        battery_set = models.Battery.objects.all()
+        lan_set = models.Lan.objects.all()
+        post_q = models.Poster.objects.all().order_by('number')
+        unbrella_count = unbrella_set.filter(Q(is_borrowed = True) | Q(is_reserved = True)).count()
+        battery_count = battery_set.filter(Q(is_borrowed = True) | Q(is_reserved = True)).count()
+        lan_count = lan_set.filter(Q(is_borrowed = True) | Q(is_reserved = True)).count()
+        message = ""
+        yes_no = False
+        ans=request.POST.get('ans', 'No')
+        if lan_count < lan_set.count():
+            for item in lan_set:
+                    if item.is_borrowed == False and item.is_reserved == False :
+                        break
+            if request.method == "GET":
+                message = str(item.number)+"번 랜선을 빌리시겠습니까?"
+                yes_no = True
+            else :
+                if ans=='Yes':
+                    item.borrowed_by = request.user.user_data
+                    item.is_reserved = True
+                    item.reservation_time = timezone.localtime()
+                    item.save()
+                    logger.info('랜선 사업 : [학번:'+request.user.username+'| 랜선 번호:'+str(item.number)+'] 대여 완료') # 담당자:{}
+                return redirect('login:main')
         else :
-            if ans=='Yes':
-                item.borrowed_by = request.user.user_data
-                item.is_borrowed = True
-                item.save()
-            return redirect('login:main')
-    else :
-        message = "현재 대여 가능한 랜선이 없습니다."
-        if ans=="OK":
-            return redirect('login:main')
+            message = "현재 예약 가능한 랜선이 없습니다."
+            if ans=="OK":
+                return redirect('login:main')
+            yes_no = False
         return render(request, 'login/main_lendlan.html', {
             'message': message,
-            'yesno': False,
+            'yesno': yes_no,
             'unbrella_count':unbrella_count,
             'battery_count':battery_count,
+            'posts':post_q,
             'battery_total' : battery_set.count(),
             'unbrella_total': unbrella_set.count(),
             'lan_total': lan_set.count(),
@@ -353,3 +319,27 @@ class PasswordChangeView(PasswordContextMixin, FormView):
         # except the current one.
         update_session_auth_hash(self.request, form.user)
         return super().form_valid(form)
+
+
+# 우산의 번호를 받아 우산을 반환 (Post 방식)
+# 만약 프로그램 내에서 변경이 불가하다면, 이를 이용 (나머지는 같은 방식으로 이름만 조금 바꾸자.)
+def ReturnUnbrella(request):
+    if request.method == "POST":
+        num = request.POST['num']
+        item_q = models.Unbrella.objects.all() # 오직 하나의 객체
+        if int(num) <= item_q.count() and int(num) >= 0 :
+            item_q = models.Unbrella.objects.all().filter(number=num) # 오직 하나의 객체
+            for item in item_q :
+                if item.is_borrowed == True :
+                    item.is_borrowed = False
+                    item.borrowed_time = timezone.localtime()
+                    item.borrowed_by = None
+                    item.save()
+                else:
+                    print("해당 우산은 대여되지 않았습니다.")
+        else:
+            print("올바른 우산 번호가 아닙니다.")
+    return redirect('login:main')
+
+
+
